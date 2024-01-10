dd1da77cf9a6f421a7db9359f1814ba70eb17203
lyy4758 <1457910223@qq.com>
2024-01-10 15:54:14 +0800

    right

---
+++ lua/first_plugin/extra/bufferline/right.lua
@@ -0,0 +1,10 @@
+-- -@diagnostic disable-next-line: unused-local
+return function()
+	return function()
+		return {
+			{ text = "  ", bg = "#252526", fg = "#FFFFFF" },
+			{ text = "  ", bg = "#252526", fg = "#FFFFFF" },
+			{ text = "  ", bg = "#252526", fg = "#FFFFFF" },
+		}
+	end
+end
--- lua/first_plugin/init.lua
+++ lua/first_plugin/init.lua
@@ -1,17 +1,31 @@
-local default_config = require('first_plugin.conf')
-local core = require('first_plugin.core')
-local test = require('first_plugin.test')
-local M = {}
+local default_config = require("first_plugin.conf")
+local core = require("first_plugin.core")
+local test = require("first_plugin.test")
+local M = {
+	_conf = {},
+	_colors = {},
+}
+function M.get_config()
+	return M._conf
+end
+function M.get_color()
+	return M._colors
+end
 test.begin()
-M.info =  "this is my first neovim plugin"
+M.info = "this is my first neovim plugin"
 function M.setup(opts)
-  vim.notify(M.info,"info")
-  -- vim.print(default_config)
-  -- vim.print(opts)
+	vim.notify(M.info, "info")
+	-- vim.print(default_config)
+	-- vim.print(opts)
 
-  -- user config overwrite default config
-  local conf = vim.tbl_deep_extend("force", default_config, opts or {})
-  core.set_normal_background(conf.color,conf.style)
-  -- vim.print(conf)
+	-- user config overwrite default config
+	local conf = vim.tbl_deep_extend("force", default_config, opts or {})
+	core.set_normal_background(conf.color, conf.style)
+	-- vim.print(conf)
+end
+
+function M.get_bufferline_right()
+	vim.notify("get_bufferline_right", "info")
+	return require("first_plugin.extra.bufferline.right")()
 end
 return M
